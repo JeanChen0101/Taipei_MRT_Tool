0ec18c01672aad02ace8f2864427bb31707f20e6
A0884 Jean <rong_chen@provision.com.tw>
2023-11-01 17:35:57 +0800

    Initial commit

---
+++ src/main/java/com/tpe/mrt/tool/controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.tpe.mrt.tool.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/home")
+    public String welcomePage() {
+        return "welcome";
+    }
+
+
+
+
+}
